f018bed44bd0e91ed594fc718f324cc71030aeed
Alpha TAN <alphatan@users.noreply.github.com>
2017-10-27 08:09:37 +0800

    1) add an example of integrating spriingmvc and mybatis with xml configuration ;

---
+++ spring-mvc-mybatis-xml/src/main/java/com/at/spring/mybatis/dao/BlogDao.java
@@ -0,0 +1,55 @@
+package com.at.spring.mybatis.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.at.spring.mybatis.vo.Blog;
+
+
+@Repository
+public class BlogDao extends SqlSessionDaoSupport {
+	private static final Logger logger = LoggerFactory.getLogger(BlogDao.class);
+
+	@Override
+	@Autowired
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate){
+		super.setSqlSessionTemplate(sqlSessionTemplate);
+	}
+	
+	@Override
+	@Autowired
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory){
+		super.setSqlSessionFactory(sqlSessionFactory);
+	}
+	
+	
+	public List<Blog> listPage(Map<String, Object> paramMap, String sqlId) {
+		logger.debug("paramMap: '" + paramMap +"'");
+		if (paramMap == null)
+			paramMap = new HashMap<String, Object>();
+
+		List<Blog> list = getSqlSession().selectList("com.at.spring.mybatis.mapper.BlogMapper."+sqlId, paramMap, new RowBounds(3,3));
+
+		return list;
+	}
+	
+	public Blog listOne(Map<String, Object> paramMap, String sqlId) {
+		logger.debug("paramMap: '" + paramMap +"'");
+		if (paramMap == null)
+			paramMap = new HashMap<String, Object>();
+
+		Blog blog = getSqlSession().selectOne("com.at.spring.mybatis.mapper.BlogMapper."+sqlId, paramMap);
+
+		return blog;
+	}
+}
---
+++ spring-mvc-mybatis-xml/src/main/webapp/WEB-INF/views/jsp/in/tb/reporting.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="<c:url value="/common/js/jquery-2.1.4.js" />" type="application/javascript" ></script>
+</head>
+<body>reporting page. <c:out value="${message}" default="No Message" />
+</body>
+</html>
