e59ed6e82d774f19f79e7b168cf806ab039b3164
Alpha TAN <alphatan@users.noreply.github.com>
2017-11-30 13:34:53 +0800

    1) component-scan beans instead of define it one-by-one ;

--- dubbo-simple-service/src/main/java/com/at/dubbo/simple/service/impl/DemoServiceImpl.java
+++ dubbo-simple-service/src/main/java/com/at/dubbo/simple/service/impl/DemoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.at.dubbo.simple.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.at.dubbo.simple.service.DemoService;
 
+@Service
 public class DemoServiceImpl implements DemoService {
 	public String sayHello(String name) {
 		return "Hello " + name;
