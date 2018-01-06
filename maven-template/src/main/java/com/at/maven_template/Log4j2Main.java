3067b9c6b1781beac7ccf6dd1591dff20777841d
Alpha TAN <alphatan@users.noreply.github.com>
2018-01-06 16:07:46 +0800

    1) remove Log4j2Main.java from lombok project ; 2) add maven-template project ;

---
+++ maven-template/src/main/java/com/at/maven_template/Log4j2Main.java
@@ -0,0 +1,15 @@
+package com.at.maven_template;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Log4j2Main {
+	public static void main(String[] args) {
+		log.trace("trace");
+		log.debug("debug");
+		log.info("info");
+		log.warn("warn");
+		log.error("error");
+//		log.fatal("fatal"); // log4j-specific
+	}
+}
