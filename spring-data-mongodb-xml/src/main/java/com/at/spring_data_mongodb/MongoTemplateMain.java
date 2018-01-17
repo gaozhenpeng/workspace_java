16f5a9e115adb98a14a84f617603b44e9224cd58
Alpha TAN <alphatan@users.noreply.github.com>
2018-01-17 15:41:07 +0800

    1) add an example of invoking mongoTemplate.executeCommand(..) directly ;

--- spring-data-mongodb-xml/src/main/java/com/at/spring_data_mongodb/MongoTemplateMain.java
+++ spring-data-mongodb-xml/src/main/java/com/at/spring_data_mongodb/MongoTemplateMain.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapreduce.MapReduceOptions;
 import org.springframework.data.mongodb.core.mapreduce.MapReduceResults;
@@ -15,10 +18,15 @@ import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import org.springframework.util.ResourceUtils;
 
 import com.at.spring_data_mongodb.model.MallIndustryStatics;
 import com.mongodb.BasicDBObject;
+import com.mongodb.CommandResult;
 import com.mongodb.DBCollection;
+import com.mongodb.MapReduceCommand;
+import com.mongodb.MapReduceCommand.OutputType;
+import com.mongodb.ReadPreference;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,12 +39,18 @@ public class MongoTemplateMain {
     // inject the actual template
     @Autowired
     private MongoTemplate mongoTemplate;
+    
+    @Autowired
+    private ApplicationContext applicationContext;
 
-    public void mapreduce(Long docNum) {
-        log.info("map reduce 'copy' start:");
+    public void mapreduce_returnAll(Long docNum) {
+        log.info("mapreduce_returnAll start:");
         Assert.notNull(mongoTemplate, "mongoTemplate should not be null");
         Assert.notNull(docNum, "docNum should not be null");
-
+        
+        
+        long startTime = System.currentTimeMillis();
+        
         Query query = new BasicQuery("{}").limit(1);
 //        Query query=new Query();
 //        String createTime = "";
@@ -65,9 +79,111 @@ public class MongoTemplateMain {
                 , mapReduceOptions
                 , MallIndustryStatics.class);
         
-        log.info("map reduce 'copy' end;");
+        
+        
+        // !!!! WARNING !!!!
+        // 
+        //      mapReduceResults contains ALL the records in OUTPUT_COLLECTION !!!!
+        //
+        // !!!! WARNING !!!!
+        Assert.notNull(mapReduceResults, "mapReduceResults should not be null");
+        
+        
+        // clean variables
+        mapReduceResults = null;
+        
+        
+        long endTime = System.currentTimeMillis();
+        log.info("mapreduce_returnAll consumed ms: {}", endTime - startTime);
+        
+        
+        log.info("mapreduce_returnAll end;");
     }
+
+    public void mapreduce_noReturn(Long docNum) {
+        log.info("mapreduce_noReturn start:");
+        Assert.notNull(mongoTemplate, "mongoTemplate should not be null");
+        Assert.notNull(docNum, "docNum should not be null");
+        
+        
+        long startTime = System.currentTimeMillis();
+        
+        Query query = new BasicQuery("{}").limit(1);
+//        Query query=new Query();
+//        String createTime = "";
+//        List<String> typeList = new ArrayList<>();
+//        query.addCriteria(
+//                Criteria
+//                    .where("createTime").is(createTime)
+//                    .and("type").in(typeList)
+//        );
+        Map<String, Object> scopeVariables = new HashMap<>();
+        scopeVariables.put("doc_num", docNum);
+        
+        MapReduceCommand mrc = new MapReduceCommand(
+                mongoTemplate.getCollection(INPUT_COLLECTION)
+                ,replaceWithResourceIfNecessary("classpath:mapreduce/mall_industry_statics_cp_map.js")
+                ,replaceWithResourceIfNecessary("classpath:mapreduce/mall_industry_statics_cp_reduce.js")
+                ,OUTPUT_COLLECTION
+                ,OutputType.MERGE
+                ,query.getQueryObject()
+                );
+        mrc.setFinalize(replaceWithResourceIfNecessary("classpath:mapreduce/mall_industry_statics_cp_finalize.js"));
+        mrc.setScope(scopeVariables);
+        CommandResult commandResult = mongoTemplate.executeCommand(mrc.toDBObject(), ReadPreference.primary());
+        
+        
+        // commandResult is very clean
+        // e.g. 
+        //     commandResult: 
+        //        { "result" : "mall_industry_statics2" , "timeMillis" : 2645 , "timing" : { "mapTime" : 83 , "emitLoop" : 758 , "reduceTime" : 882 , "mode" : "mixed" , "total" : 2645} , "counts" : { "input" : 1 , "emit" : 10000 , "reduce" : 0 , "output" : 20000} , "ok" : 1.0}
+        Assert.notNull(commandResult, "commandResult should not be null");
+        
+        
+        
+        // clean variables
+        commandResult = null;
+        
+        
+        
+        long endTime = System.currentTimeMillis();
+        log.info("mapreduce_noReturn consumed ms: {}", endTime - startTime);
+        
+        
+        log.info("mapreduce_noReturn end;");
+    }
+    /**
+     * <p>If <strong>str</strong> specified a resource, then return the content of the resource, or return the <strong>str</strong> itself.
+     * </p>
+     * <p>e.g. <br />
+     * str="classpath:mapreduce/map.js", then calling replaceWithResourceIfNecessary(str) would return the content of map.js
+     * </p>
+     * @param <strong>str</strong>
+     * @return <strong>str</strong> or the content of the resource specified by <strong>str</strong>
+     */
+    public String replaceWithResourceIfNecessary(String str) {
+        String func = str;
+        if (applicationContext != null && ResourceUtils.isUrl(str)) {
+            Resource functionResource = applicationContext.getResource(func);
+            if (!functionResource.exists()) {
+                throw new RuntimeException(String.format("Resource %s not found!", str));
+            }
     
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(functionResource.getInputStream());
+                return scanner.useDelimiter("\\A").next();
+            } catch (IOException e) {
+                throw new RuntimeException(String.format("Cannot read map-reduce file %s!", str), e);
+            } finally {
+                if (scanner != null) {
+                    scanner.close();
+                }
+            }
+        }
+    
+        return func;
+    }
     
     /**
      * db.getCollection('mall_industry_statics').save({
@@ -120,9 +236,12 @@ public class MongoTemplateMain {
         long startTime = System.currentTimeMillis();
         
         MongoTemplateMain main = ctx.getBean(MongoTemplateMain.class);
+//        main.dropCollection(OUTPUT_COLLECTION);
+//        main.createCollection(OUTPUT_COLLECTION);
+//        main.mapreduce_returnAll(6000000L);
         main.dropCollection(OUTPUT_COLLECTION);
         main.createCollection(OUTPUT_COLLECTION);
-        main.mapreduce(10000L);
+        main.mapreduce_noReturn(6000000L);
         main.findMallIndustryStatics();
 
         long endTime = System.currentTimeMillis();
