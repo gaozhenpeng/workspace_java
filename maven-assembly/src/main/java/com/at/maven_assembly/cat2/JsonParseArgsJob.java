d26b28c3ef5134047fa90d1d9e6720655f0ec46a
Alpha TAN <alphatan@users.noreply.github.com>
2018-01-31 11:00:13 +0800

    1) update the project structure of assembling azkaban jobs ;

---
+++ maven-assembly/src/main/java/com/at/maven_assembly/cat2/JsonParseArgsJob.java
@@ -0,0 +1,24 @@
+package com.at.maven_assembly.cat2;
+
+import java.util.Arrays;
+
+import org.json.JSONObject;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JsonParseArgsJob {
+    // main.args = "{\"JsonParseArgsJob\" : {\"somekey\" : \"somevalue\" }, \"JsonParseArgsJob1\" : {\"otherkey\" : \"othervalue\"}}"
+    public static void main(String[] args) {
+        if (args != null && args.length > 0) {
+            Arrays.stream(args).forEach((arg) -> {
+                log.info("arg: '{}'", arg);
+            });
+            
+            // parse the first argument
+            JSONObject json = new JSONObject(args[0]);
+            JSONObject jsonParseArgsJob = json.getJSONObject(JsonParseArgsJob.class.getSimpleName());
+            log.info("'{}' of args[0]: '{}' ", JsonParseArgsJob.class.getSimpleName(), jsonParseArgsJob.toString(2));
+        }
+    }
+}
