81b0c1d599cf5db27954eeb955827cf8bab9053d
Alpha TAN <alphatan@users.noreply.github.com>
2018-03-06 09:08:52 +0800

    1) TextToParquetWithAvro.java ; 2) update configuration files of hadoop cluster in win env ;

--- hadoop/src/main/java/com/at/hadoop/io/TextPairFirstComparator.java
+++ hadoop/src/main/java/com/at/hadoop/io/TextPairFirstComparator.java
@@ -8,28 +8,29 @@ import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.io.WritableUtils;
 
 public class TextPairFirstComparator extends WritableComparator {
-	private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
+    private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
 
-	public TextPairFirstComparator(){
-		super(TextPair.class);
-	}
+    public TextPairFirstComparator() {
+        super(TextPair.class);
+    }
 
-	@Override
-	public int compare(byte[] b1, int start1, int length1, byte[] b2, int start2, int length2){
-		try{
-			int firstL1 = WritableUtils.decodeVIntSize(b1[start1]) + WritableComparator.readVInt(b1, start1);
-			int firstL2 = WritableUtils.decodeVIntSize(b2[start2]) + WritableComparator.readVInt(b2, start1);
-			return TEXT_COMPARATOR.compare(b1, start1, firstL1, b2, start2, firstL2);
-		}catch(IOException e){
-			throw new IllegalArgumentException(e);
-		}
-	}
-	
-	@Override
-	public int compare(WritableComparable a, WritableComparable b){
-		if( a instanceof TextPair && b instanceof TextPair){
-			return ((TextPair) a).getFirst().compareTo(((TextPair) b).getFirst());
-		}
-		return super.compare(a, b);
-	}
+    @Override
+    public int compare(byte[] b1, int start1, int length1, byte[] b2, int start2, int length2) {
+        try {
+            int firstL1 = WritableUtils.decodeVIntSize(b1[start1]) + WritableComparator.readVInt(b1, start1);
+            int firstL2 = WritableUtils.decodeVIntSize(b2[start2]) + WritableComparator.readVInt(b2, start1);
+            return TEXT_COMPARATOR.compare(b1, start1, firstL1, b2, start2, firstL2);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        if (a instanceof TextPair && b instanceof TextPair) {
+            return ((TextPair) a).getFirst().compareTo(((TextPair) b).getFirst());
+        }
+        return super.compare(a, b);
+    }
 }
