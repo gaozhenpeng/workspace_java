877ae55bfff8700ce314697268d196d37cff0eba
Alpha TAN <alphatan@users.noreply.github.com>
2018-03-16 15:55:09 +0800

    1) load the parent pom.xml from workspace ; 2) import spring-boot-starter-parent as dependencyManagement ; 3) add a SecondMainApp.java with main method ; 4) define start-class explicitly ;

--- spring-boot-demo/src/main/java/com/at/spring_boot/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/com/at/spring_boot/demo/DemoApplication.java
@@ -3,10 +3,14 @@ package com.at.spring_boot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.info("first main2");
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
