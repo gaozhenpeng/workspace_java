4604b9cf3d083c9b25428e184c582e3e0cbb9da6
Alpha TAN <alphatan@users.noreply.github.com>
2018-04-08 14:26:00 +0800

    1) add @ComponentScan to Application.java ;

--- dubbo-spring-boot-demo-consumer/src/main/java/com/at/spring/Application.java
+++ dubbo-spring-boot-demo-consumer/src/main/java/com/at/spring/Application.java
@@ -2,11 +2,13 @@ package com.at.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan("com.at.spring")
 public class Application {
 
     public static void main(String[] args) {
--- spring-boot-demo/src/main/java/com/at/spring_boot/demo/Application.java
+++ spring-boot-demo/src/main/java/com/at/spring_boot/demo/Application.java
@@ -2,15 +2,17 @@ package com.at.spring_boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan("com.at.spring_boot")
 public class Application {
 
     public static void main(String[] args) {
-        log.info("first main2");
+        log.info("spring application main");
         SpringApplication.run(Application.class, args);
     }
 }
