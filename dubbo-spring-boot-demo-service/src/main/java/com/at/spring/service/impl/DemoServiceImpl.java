f82ae052503b74a2040960446fb4918692d2311c
Alpha TAN <alphatan@users.noreply.github.com>
2018-04-08 09:50:48 +0800

    1) remove the 'application', 'protocol', 'registry' from service implementation (use the ones in application.properties) ;

--- dubbo-spring-boot-demo-service/src/main/java/com/at/spring/service/impl/DemoServiceImpl.java
+++ dubbo-spring-boot-demo-service/src/main/java/com/at/spring/service/impl/DemoServiceImpl.java
@@ -3,12 +3,7 @@ package com.at.spring.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.at.spring.service.DemoService;
 
-@Service(
-        version = "1.0.0",
-        application = "${dubbo.application.id}",
-        protocol = "${dubbo.protocol.id}",
-        registry = "${dubbo.registry.id}"
-)
+@Service(version = "1.0.0")
 public class DemoServiceImpl implements DemoService {
 
     public String sayHello(String name) {
