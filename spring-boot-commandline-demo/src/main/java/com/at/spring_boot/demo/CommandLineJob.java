4d963d372eeeefb13b4920d3e5d049dcf508ed36
Alpha TAN <alphatan@users.noreply.github.com>
2018-05-03 16:45:43 +0800

    1) an example of spring-boot commandline ;

---
+++ spring-boot-commandline-demo/src/main/java/com/at/spring_boot/demo/CommandLineJob.java
@@ -0,0 +1,28 @@
+package com.at.spring_boot.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Order(100)
+public class CommandLineJob implements CommandLineRunner{
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Entering CommandLineJob run, args: '{}'", Arrays.toString(args));
+        
+
+        log.info("I'm doing a hard work!");
+        
+        
+        
+        log.info("Leaving CommandLineJob run.");
+    }
+
+}
