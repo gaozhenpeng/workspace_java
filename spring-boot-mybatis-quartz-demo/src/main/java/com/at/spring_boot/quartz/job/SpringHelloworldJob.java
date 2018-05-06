b4f6fe9ae9f5c2db4820c065325f3ebf82e8803c
Alpha TAN <alphatan@users.noreply.github.com>
2018-05-06 22:09:04 +0800

    1) an example of integrating spring-boot, mybatis, web and quartz (completely copied from the projects of 'spring-boot-web-mybatis-demo' and 'spring-quartz') ;

---
+++ spring-boot-mybatis-quartz-demo/src/main/java/com/at/spring_boot/quartz/job/SpringHelloworldJob.java
@@ -0,0 +1,74 @@
+package com.at.spring_boot.quartz.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
+import org.quartz.Trigger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+// Note that
+//     It would disable concurrent execution ONLY in ONE scheduler.
+//     If multiple schedulers have been defined, the same job would 
+//     also be executed concurrently.
+//     This is implemented by using lock table 'qrtz_locks' in database
+@DisallowConcurrentExecution
+@PersistJobDataAfterExecution
+public class SpringHelloworldJob extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        final JobDetail jobDetail = jobExecutionContext.getJobDetail();
+        final Trigger trigger = jobExecutionContext.getTrigger();
+        log.info("Helloworld!, jobDetail: '{}', trigger: '{}'", jobDetail, trigger);
+    }
+
+    
+    
+    
+    
+    @Bean(name="springhellworldjob")
+    public JobDetailFactoryBean springHelloworld() {
+        JobDetailFactoryBean bean = new JobDetailFactoryBean();
+        bean.setJobClass(SpringHelloworldJob.class);
+        bean.setDurability(true);
+        bean.setRequestsRecovery(true);
+        bean.setName("springhellworldjob");
+        bean.setGroup("springhellworldjob_group");
+        return bean;
+    }
+
+    @Bean(name="springhellworldjob_crontrigger")
+    public CronTriggerFactoryBean dailySummarytrigger() {
+        CronTriggerFactoryBean bean = new CronTriggerFactoryBean();
+        bean.setJobDetail(springHelloworld().getObject());
+        bean.setStartDelay(5000);
+        bean.setBeanName("springhellworldjob_crontrigger");
+        bean.setGroup("springhellworldjob_group");
+        //                      sec min hou day mon wee year
+        bean.setCronExpression("0/3   *   *  *   *   ?   *");
+        return bean;
+    }
+
+    @Bean(name="springhellworldjob_simpletrigger")
+    public SimpleTriggerFactoryBean simpleTrigger() {
+        SimpleTriggerFactoryBean bean = new SimpleTriggerFactoryBean();
+        bean.setJobDetail(springHelloworld().getObject());
+        bean.setBeanName("springhellworldjob_simpletrigger");
+        bean.setGroup("springhellworldjob_group");
+        bean.setRepeatInterval(2000); // repeat interval, in miliseconds
+        bean.setRepeatCount(19); // total execution = repeat count + 1
+        return bean;
+    }
+
+}
---
+++ spring-boot-mybatis-quartz-demo/src/main/java/com/at/spring_boot/web/ctrl/BlogController3.java
@@ -0,0 +1,45 @@
+package com.at.spring_boot.web.ctrl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.at.spring_boot.mybatis.dto.BlogDto;
+import com.at.spring_boot.web.svr3.BlogService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/blog3")
+public class BlogController3 {
+    @Autowired
+    @Qualifier("blogService3")
+    private BlogService blogService;
+
+    @RequestMapping(method=RequestMethod.POST)
+    public BlogDto create(
+            @RequestParam(name="name", required=true) String name
+            , @RequestParam(name="content", required=false, defaultValue="") String content
+            ) {
+        log.info("create blog, name: '{}', content: '{}' ", name, content);
+        
+        BlogDto blogDto = blogService.create(name, content);
+        return blogDto;
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public List<BlogDto> list(
+            @RequestParam(name="blogId", required=false) Long blogId
+            ) {
+        log.info("list blog, blogId: '{}' ", blogId);
+        
+        List<BlogDto> blogDtos = blogService.list(blogId);
+        return blogDtos;
+    }
+}
