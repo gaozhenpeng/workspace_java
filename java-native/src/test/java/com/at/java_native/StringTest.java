4a7fce0dac1baa5e7d8be5d9888ece1155eae89b
Alpha TAN <alphatan@users.noreply.github.com>
2018-05-29 15:03:54 +0800

    1) add StringTest.java ;

---
+++ java-native/src/test/java/com/at/java_native/StringTest.java
@@ -0,0 +1,18 @@
+package com.at.java_native;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StringTest {
+    @Test
+    public void testString() {
+        List<Integer> idList = Arrays.asList(1,2,3,4,5);
+        
+        log.info(String.format("'%s'", idList));
+    }
+}
