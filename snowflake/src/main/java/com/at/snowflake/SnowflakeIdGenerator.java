1d18749372d72e767c898d8bc62897c0224fec61
Alpha TAN <alphatan@users.noreply.github.com>
2018-05-30 09:26:00 +0800

    1) refactor to change the position of fields ; 2) change the start time to '2018-01-01 00:00:00.000' ;

--- snowflake/src/main/java/com/at/snowflake/SnowflakeIdGenerator.java
+++ snowflake/src/main/java/com/at/snowflake/SnowflakeIdGenerator.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 0 - 12345678 12345678 12345678 12345678 12345678 1 - 12345678 12 - 12345678 1234
  *   * 0
- *      unused
+ *       unused
  *   * 12345678 12345678 12345678 12345678 12345678 1
  *       41 bits of timestamp in miliseconds
  *          System.currentTimeMillis: 64bits
@@ -33,16 +33,10 @@ import lombok.extern.slf4j.Slf4j;
  */
 public class SnowflakeIdGenerator {
     
-    public SnowflakeIdGenerator(long dataCenterId, long workerId) {
-        super();
-        this.dataCenterId = dataCenterId;
-        this.workerId = workerId;
-    }
-
     /**
-     * start miliseconds (2015-01-01)
+     * start miliseconds (2018-01-01 00:00:00.000 = 1514736000000)
      */
-    private final long START_TIME_IN_MS = 1420041600000L;
+    private final long START_TIME_IN_MS = 1514736000000L;
 
     private final long MACHINE_ID_BITS = 5L;
     private final long DATACENTER_ID_BITS = 5L;
@@ -52,6 +46,20 @@ public class SnowflakeIdGenerator {
     private final long DATACENTER_ID_LEFT_SHIFT_BITS = SEQUENCE_BITS + MACHINE_ID_BITS;
     private final long TIMESTAMP_LEFT_SHIFT_BITS = SEQUENCE_BITS + MACHINE_ID_BITS + DATACENTER_ID_BITS;
 
+    /** 0b1111 1111 1111 = 0xFFF */
+    private final long SEQUENCE_MASK = 0xFFF;
+    /** 0b1 1111 = 0x1F */
+    private final long WORKER_ID_MASK = 0x1F;
+    /** 0b1 1111 = 0x1F */
+    private final long DATA_CENTER_ID_MASK = 0x1F;
+    /** 0b 1 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 = 0x1FFFFFFFFFF */
+    private final long TIMESTAMP_MASK = 0x1FFFFFFFFFFL;
+    
+    
+    /** lock for sequential */
+    private static final ReentrantLock reentrantLock = new ReentrantLock();
+    
+
     
     
     private long dataCenterId;
@@ -59,15 +67,12 @@ public class SnowflakeIdGenerator {
     
     private long lastSequenceNum = 0L;
     private long lastTimestamp = -1L;
-    
-    /** 0b1111 1111 1111 = 0xFFF */
-    private final long SEQUENCE_MASK = 0xFFF;
-    private final long WORKER_ID_MASK = 0x1F;
-    private final long DATA_CENTER_ID_MASK = 0x1F;
-    /** 0b 1 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 = 0x1FFFFFFFFFF */
-    private final long TIMESTAMP_MASK = 0x1FFFFFFFFFFL;
-    
-    private static final ReentrantLock reentrantLock = new ReentrantLock();
+
+    public SnowflakeIdGenerator(long dataCenterId, long workerId) {
+        super();
+        this.dataCenterId = dataCenterId;
+        this.workerId = workerId;
+    }
 
     public long nextId() throws InterruptedException {
         reentrantLock.lock();
