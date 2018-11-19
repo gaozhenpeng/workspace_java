4581795a54a2072dcd5aca68fc3fb4aa6931c71f
Alpha TAN <alphatan@users.noreply.github.com>
2018-11-19 08:16:51 +0800

    1) set the plugin versions in the place instead of referencing properties ;

--- apache-log4j2/src/main/java/com/at/log4j2/Log4j2NativeMain.java
+++ apache-log4j2/src/main/java/com/at/log4j2/Log4j2NativeMain.java
@@ -4,14 +4,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log4j2NativeMain {
-	private final static Logger logger = LogManager.getLogger(Log4j2Main.class); // log4j2
+    private final static Logger logger = LogManager.getLogger(Log4j2Main.class); // log4j2
 
-	public static void main(String[] args) {
-		logger.trace("trace");
-		logger.debug("debug");
-		logger.info("info");
-		logger.warn("warn");
-		logger.error("error");
-//		logger.fatal("fatal"); // log4j-specific
-	}
+    public static void main(String[] args) {
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        logger.fatal("fatal"); // log4j-specific
+    }
 }
