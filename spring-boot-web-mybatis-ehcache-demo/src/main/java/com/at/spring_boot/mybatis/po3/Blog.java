47a9169692857a28f78ceea710520b4f1baa6ce0
Alpha TAN <alphatan@users.noreply.github.com>
2018-11-19 14:13:30 +0800

    1) add back the comments of creating table and inserting test data ;

--- spring-boot-web-mybatis-ehcache-demo/src/main/java/com/at/spring_boot/mybatis/po3/Blog.java
+++ spring-boot-web-mybatis-ehcache-demo/src/main/java/com/at/spring_boot/mybatis/po3/Blog.java
@@ -2,6 +2,34 @@ package com.at.spring_boot.mybatis.po3;
 
 import java.util.Date;
 
+/**
+ * <pre>
+ * <code>
+ * USE test;
+ * DROP TABLE IF EXISTS `blog`;
+ * CREATE TABLE `blog` (
+ *   `blog_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+ *   `name` varchar(64) DEFAULT NULL,
+ *   `content` varchar(512) DEFAULT NULL,
+ *   `created_datetime` datetime DEFAULT null,
+ *   `updated_datetime` datetime DEFAULT null,
+ *   PRIMARY KEY (`blog_id`)
+ * ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ * 
+ * insert into `blog` (`name`, `content`, `created_datetime`, `updated_datetime`) values
+ *  ('name1', 'content1', now(), now())
+ * ,('name2', 'content2', now(), now())
+ * ,('name3', 'content3', now(), now())
+ * ,('name4', 'content4', now(), now())
+ * ,('name5', 'content5', now(), now())
+ * ;
+ * commit
+ * ;
+ * </code>
+ * </pre>
+ *
+ */
+
 public class Blog {
     private Long blogId;
 
