661acd9387f33661a713d7d16e0bcfb9e410d517
Alpha TAN <alphatan@users.noreply.github.com>
2019-01-18 14:02:16 +0800

    1) add an example of using json_column ;

--- sqlite/src/main/java/com/at/sqlite/SQLiteMain.java
+++ sqlite/src/main/java/com/at/sqlite/SQLiteMain.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLType;
 import java.sql.Statement;
 import java.sql.Timestamp;
 
@@ -75,10 +76,7 @@ public class SQLiteMain {
 		prep.setString(2, "c2sx");
 		prep.setString(3, "aliyun");
 		prep.setTimestamp(4, new Timestamp(new java.util.Date().getTime()));
-		prep.setTimestamp(5, new Timestamp(new java.util.Date().getTime())); // (4,
-																				// new
-																				// Date(new
-																				// java.util.Date().getTime()));
+		prep.setTimestamp(5, new Timestamp(new java.util.Date().getTime()));
 		prep.addBatch();
 		prep.setString(1, "ak2");
 		prep.setString(2, "c2sy");
@@ -137,10 +135,7 @@ public class SQLiteMain {
 		prep.setString(2, "c2sx");
 		prep.setString(3, "aliyun");
 		prep.setTimestamp(4, new Timestamp(new java.util.Date().getTime()));
-		prep.setTimestamp(5, new Timestamp(new java.util.Date().getTime())); // (4,
-																				// new
-																				// Date(new
-																				// java.util.Date().getTime()));
+		prep.setTimestamp(5, new Timestamp(new java.util.Date().getTime()));
 		prep.addBatch();
 		prep.setString(1, "ak2");
 		prep.setString(2, "c2sy");
@@ -171,11 +166,57 @@ public class SQLiteMain {
 		connTest.close();
 	}
 
+    public void json_column() throws SQLException {
+
+        Connection connTest = DriverManager.getConnection("jdbc:sqlite::memory:");
+        Statement stat = connTest.createStatement();
+        // cleanup
+        stat.executeUpdate("drop table if exists `json_user`;");
+
+        // create table
+        stat.executeUpdate("" + " CREATE TABLE `json_user` ( "
+                + "   `id` integer /* unsigned bigint */ NOT NULL PRIMARY KEY AUTOINCREMENT  "
+                + "   ,`user_name` varchar(20) DEFAULT NULL /* COMMENT 'user name' */ "
+                + "   ,`last_login_info` json DEFAULT NULL /* COMMENT 'the info of last login' */ "
+                + " )");
+
+//        // create index
+//        stat.executeUpdate(
+//                "CREATE INDEX IF NOT EXISTS `vc3_cloudgateway_key_cloudtype_idx` on `vc3_cloudgateway_key` (`cloudtype`);");
+
+        //
+        PreparedStatement prep = connTest.prepareStatement(
+                "insert into `json_user` (`user_name`, `last_login_info`) values (?, ?) ");
+        prep.setString(1, "lucy");
+        prep.setString(2, "{\"time\":\"2015-01-01 13:00:00\",\"ip\":\"192.168.1.1\",\"result\":\"fail\"}");
+        prep.addBatch();
+        prep.setString(1, "bob");
+        prep.setString(2, "{\"time\":\"2015-01-07 06:44:00\",\"ip\":\"192.168.1.4\",\"result\":\"success\"}");
+        prep.addBatch();
+        prep.setString(1, "lucy");
+        prep.setString(2, "{\"time\":\"2016-07-08 09:44:00\",\"ip\":\"192.168.1.44\",\"result\":\"success\"}");
+        prep.addBatch();
+
+        connTest.setAutoCommit(false);
+        prep.executeBatch();
+        connTest.setAutoCommit(true);
+
+        ResultSet rs = stat.executeQuery("select * from `json_user` where JSON_EXTRACT(last_login_info, '$.result') = 'success' ;");
+        while (rs.next()) {
+            log.info("id = " + rs.getInt("id"));
+            log.info("user_name = " + rs.getString("user_name"));
+            log.info("last_login_info = " + rs.getString("last_login_info"));
+        }
+        rs.close();
+        connTest.close();
+    }
+
 	public static void main(String[] args) throws Exception {
 		Class.forName("org.sqlite.JDBC");
 		SQLiteMain sqliteMain = new SQLiteMain();
 		sqliteMain.featured();
 		sqliteMain.mysql();
-		sqliteMain.memory();
+        sqliteMain.memory();
+		sqliteMain.json_column();
 	}
 }
