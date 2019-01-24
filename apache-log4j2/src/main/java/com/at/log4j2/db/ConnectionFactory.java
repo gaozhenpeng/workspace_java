9858e381f83b3ffed8533120a8a7dd5c9cf74ec2
Alpha TAN <alphatan@users.noreply.github.com>
2019-01-24 10:58:29 +0800

    1) use hikaricp to send logs to database ;

--- apache-log4j2/src/main/java/com/at/log4j2/db/ConnectionFactory.java
+++ apache-log4j2/src/main/java/com/at/log4j2/db/ConnectionFactory.java
@@ -2,45 +2,28 @@ package com.at.log4j2.db;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
-import org.apache.commons.dbcp2.PoolableConnection;
-import org.apache.commons.dbcp2.PoolableConnectionFactory;
-import org.apache.commons.dbcp2.PoolingDataSource;
-import org.apache.commons.pool2.impl.GenericObjectPool;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectionFactory {
-	private static interface Singleton {
-		final ConnectionFactory INSTANCE = new ConnectionFactory();
-	}
+    private static final DataSource DATASOURCE;
+    static {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(
+                "jdbc:mysql://127.0.0.1:3306/log4j2?useSSL=false&serverTimezone=UTC&nullCatalogMeansCurrent=true");
+        hikariConfig.setUsername("log4j2");
+        hikariConfig.setPassword("log4j2");
+        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 
-	private final DataSource dataSource;
+        DATASOURCE = new HikariDataSource(hikariConfig);
+    }
 
-	private ConnectionFactory() {
-		Properties properties = new Properties();
-		properties.setProperty("user", "log4j2");
-		properties.setProperty("password", "log4j2"); // or get properties from some configuration file
-
-		DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
-				"jdbc:mysql://127.0.0.1:3306/log4j2?useSSL=false", properties);
-		GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-		genericObjectPoolConfig.setMaxIdle(10);
-		genericObjectPoolConfig.setMinIdle(3);
-		genericObjectPoolConfig.setMaxWaitMillis(120000); // 120s
-		
-		GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<PoolableConnection>(
-				new PoolableConnectionFactory(connectionFactory, null)
-				,genericObjectPoolConfig
-				);
-
-		this.dataSource = new PoolingDataSource<PoolableConnection>(pool);
-	}
-
-	public static Connection getDatabaseConnection() throws SQLException {
-		return Singleton.INSTANCE.dataSource.getConnection();
-	}
+    public static Connection getDatabaseConnection() throws SQLException {
+        return DATASOURCE.getConnection();
+    }
 }
