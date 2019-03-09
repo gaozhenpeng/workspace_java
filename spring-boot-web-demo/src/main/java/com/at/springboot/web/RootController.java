00e1934f1c424ef597aacbd29e327ef50021a229
Alpha TAN <alphatan@users.noreply.github.com>
2019-03-09 19:54:03 +0800

    1) add an example of file uploading ; 2) delete the unnecessary class, SecondMainApp.java ;

--- spring-boot-web-demo/src/main/java/com/at/springboot/web/RootController.java
+++ spring-boot-web-demo/src/main/java/com/at/springboot/web/RootController.java
@@ -1,8 +1,11 @@
 package com.at.springboot.web;
 
+import org.apache.logging.log4j.message.FormattedMessage;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +30,32 @@ public class RootController {
         mav.addObject("message", "hwmv 你好吗？？？");
 
         log.debug("Exiting hwmv");
+        return mav;
+    }
+
+    @RequestMapping(value="/upload")
+    public ModelAndView upload(
+            @RequestParam(value = "myfile", required=false) MultipartFile myfile
+            ,Model model) {
+        log.debug("Entering upload");
+
+        String msg = "";
+        if(myfile != null) {
+            log.info("isEmpty: '{}', name: '{}', originalFilename: '{}', size: '{}'", myfile.isEmpty(), myfile.getName(), myfile.getOriginalFilename(), myfile.getSize());
+
+            msg = new FormattedMessage(
+                        "isEmpty: '{}', name: '{}', originalFilename: '{}', size: '{}'"
+                        , myfile.isEmpty()
+                        , myfile.getName()
+                        , myfile.getOriginalFilename()
+                        , myfile.getSize()
+                    ).getFormattedMessage();
+        }
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("upload");
+        mav.addObject("message", msg);
+
+        log.debug("Exiting upload");
         return mav;
     }
 }
