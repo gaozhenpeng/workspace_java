905473be12516e7db5e760f6161750e947b2d5cb
Alpha TAN <alphatan@users.noreply.github.com>
2019-05-24 14:20:47 +0800

    1) fix typo in comments ;

--- spring-boot-cli-demo/src/main/java/com/at/springboot/cl/Application.java
+++ spring-boot-cli-demo/src/main/java/com/at/springboot/cl/Application.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * mvn clean package
- * java -jar target/spring-boot-commandline-demo-0.0.1-SNAPSHOT.jar com.at.springboot.cl.Application arg1 arg2 arg3 arg4 arg5 optname=myvalue optname=myvalue2 --optname=myvalue3 --optname=myvalue4
+ * java -jar target/spring-boot-cli-demo-0.0.1-SNAPSHOT.jar com.at.springboot.cl.Application arg1 arg2 arg3 arg4 arg5 optname=myvalue optname=myvalue2 --optname=myvalue3 --optname=myvalue4
  */
 @Slf4j
 @SpringBootApplication(scanBasePackages="com.at.springboot")
--- spring-boot-cli-scheduler-demo/src/main/java/com/at/springboot/cl/Application.java
+++ spring-boot-cli-scheduler-demo/src/main/java/com/at/springboot/cl/Application.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * mvn clean package
- * java -jar target/spring-boot-commandline-demo-0.0.1-SNAPSHOT.jar com.at.springboot.cl.Application arg1 arg2 arg3 arg4 arg5 optname=myvalue optname=myvalue2 --optname=myvalue3 --optname=myvalue4
- */
 @Slf4j
 @SpringBootApplication(scanBasePackages="com.at.springboot")
 public class Application {
