c0734ea2067857323184ad492cbe9d49b394d8c6
Alpha TAN <alphatan@users.noreply.github.com>
2019-06-09 16:57:35 +0800

    1) add regexp test ;

--- java8-native-demo/src/test/java/com/at/java8/StringTest.java
+++ java8-native-demo/src/test/java/com/at/java8/StringTest.java
@@ -15,4 +15,10 @@ public class StringTest {
         
         log.info(String.format("'%s'", idList));
     }
+    @Test
+    public void testRegex() {
+        String a = "2019-06-05T01:57:20.000Z {name=node6.xndx} NQ_AC %%10STAMGR/6/STAMGR_ROAM_SUCCESS: Client 5068-0aae-31e8 roamed from BSSID 3cf5-cc82-9d70 on AP ny4s_417 of AC IP 127.0.0.1 to BSSID 3cf5-cc82-7fd0 on AP ny4s_415 of AC IP 127.0.0.1 successfully.";
+        
+        log.info(a.replaceFirst(".*IP ([0-9.]+) successfully.$", "$1"));
+    }
 }
